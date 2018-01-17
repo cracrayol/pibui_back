67f65ebf299ea1d7a184fe5e4bfac30ce5f99193
cracrayol <cracrayol@free.fr>
2018-01-17 16:21:56 +0100

    Add search router

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import * as bodyParser from 'body-parser';
 import { userRouter } from './routers/user.router';
 import { movieRouter } from './routers/movie.router';
 import { tokenGuard } from './middlewares/token-guard';
+import { searchRouter } from './routers/search.router';
+
 const app = express();
 const port = 4300;
 
@@ -12,6 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/movie', movieRouter);
+app.use('/search', searchRouter);
 app.use('/', userRouter);
 
 // Unprotected Get
---
+++ src/routers/search.router.ts
@@ -0,0 +1,58 @@
+// src/routers/user.router.ts
+
+import { Op } from 'sequelize';
+import { Router, Request, Response } from 'express'
+import { Movie, MovieModel } from '../models/movie';
+import { sequelize } from '../instances/sequelize';
+import { Author } from '../models/author';
+import { Tag } from '../models/tag';
+import { randomBytes } from 'crypto';
+import * as Promise from "bluebird";
+
+export const searchRouter = Router();
+
+searchRouter.get('/:search', (req: Request, res: Response) => {
+    let searchList: string[] = req.params.search.split(" ");
+    const searchQueries: Promise<MovieModel[]>[] = [];
+    searchList.forEach(value => {
+        if(value.length < 4) {
+            // no search if word if below 4 characters
+            return;
+        }
+        searchQueries.push(Movie.findAll({
+            where: {
+                title: {
+                    [Op.like]: '%' + value + '%'
+                },
+                valid: true,
+                hidden: false
+            },
+            include: [Author]
+        }));
+        searchQueries.push(Movie.findAll({
+            where: {
+                valid: true,
+                hidden: false
+            },
+            include: [{
+                model: Author,
+                where: {
+                    name: {
+                        [Op.like]: '%' + value + '%'
+                    }
+                },
+            }]
+        }));
+    })
+
+    const movieList: Set<MovieModel> = new Set<MovieModel>();
+    Promise.all(searchQueries).then(result => {
+        result.forEach(resultQuery => {
+            resultQuery.forEach(movie => movieList.add(movie));
+        });
+        const movies = [...movieList];
+        res.send(movies.sort((a, b) => {
+            return a.title.localeCompare(b.title);
+        }));
+    });
+});
