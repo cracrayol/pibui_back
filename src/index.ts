171e05113d49bb8671e5a04e07cfe01dba90f125
cracrayol <cracrayol@free.fr>
2018-01-17 16:21:33 +0100

    Set default port to 4300

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,30 @@
-    // src/index.ts
+import * as express from 'express';
+import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
 
-    import * as express from 'express';
-    import * as cors from 'cors';
-    import * as bodyParser from 'body-parser';
-    import { userRouter } from './routers/user.router';
-    import { movieRouter } from './routers/movie.router';
-    import { tokenGuard } from './middlewares/token-guard';
+import { userRouter } from './routers/user.router';
+import { movieRouter } from './routers/movie.router';
+import { tokenGuard } from './middlewares/token-guard';
+const app = express();
+const port = 4300;
 
-    const app = express();
-    const port = 4001;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/movie', movieRouter);
+app.use('/', userRouter);
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(cors());
-    app.use('/movie', movieRouter);
-    app.use('/', userRouter);
+// Unprotected Get
+app.get('/some-resource', (req, res, next) => {
+    res.json('Hello World');
+})
+app.use(tokenGuard());
 
-    // Unprotected Get
-    app.get('/some-resource', (req, res, next) => {
-        res.json('Hello World');
-    })
-    app.use(tokenGuard());
+// Protected Get
+app.get('/some-protected-resource', (req, res, next) => {
+    res.json('Protected Hello World');
+});
 
-    // Protected Get
-    app.get('/some-protected-resource', (req, res, next) => {
-        res.json('Protected Hello World');
-    });
-
-    app.listen(port, () => {
-        console.log(`App is listening on port ${port}`);
-    });
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
+});
